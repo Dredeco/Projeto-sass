8695a42162182c449c704bbfe521827b2de0e2b2
André Fernandes <andrefersouza@gmail.com>
2022-07-28 23:09:06 -0300

    deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import '../styles/components/Header.sass'
 
 const Header = () => {
+
+  let [open,setOpen]=useState(false);
+
   return (
+    
     <div className="header">
 
       <div className="logo">
@@ -10,25 +15,29 @@ const Header = () => {
 
       <nav className="desktop">
         
-      <div id="icons-social">
-        <a href="https://facebook.com"><i className="fa fa-facebook" aria-hidden="true"></i></a>
-        <a href="https://instagram.com"><i className="fa fa-instagram" aria-hidden="true"></i></a>
-      </div>
+        <div id="icons-social">
+          <a href="https://facebook.com"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="https://instagram.com"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
 
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="#products">Sobre</a></li>
-        <li><a href="#team">Contato</a></li>
-      </ul>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="#products">Sobre</a></li>
+          <li><a href="#team">Contato</a></li>
+        </ul>
+
       </nav>
 
       <nav className="mobile">
-      <i className="fa fa-bars"></i>
-      <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">Sobre</a></li>
-        <li><a href="">Contato</a></li>
-      </ul>	
+
+      <i onClick={()=>setOpen(!open)} className={open ? "fa-solid fa fa-close" : "fa-solid fa fa-bars"}></i>
+
+        <ul className={`${open ? 'menu-open' : 'menu-closed' }`}>
+        <li><a href="/">Home</a></li>
+          <li><a href="#products">Sobre</a></li>
+          <li><a href="#team">Contato</a></li>
+        </ul>	
+
       </nav>
       <div className="clear"></div>
 
